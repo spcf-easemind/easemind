c9cc229981d8f9f50f42d7054e2da5b22fe67144
gabrielgatbonton <ggatbonton.cs@gmail.com>
2024-10-13 13:56:50 +0800

    Added File Input

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
-import { Title, Box } from "@mantine/core";
+import { Title, Box, FileInput, Button } from "@mantine/core";
 import { useUsersStore } from "../store/users";
 import { useEffect } from "react";
 import { useShallow } from "zustand/shallow";
+import { useForm } from "@mantine/form";
 
 export default function HomePage() {
   const { getAllUsers } = useUsersStore(
@@ -13,10 +14,40 @@ export default function HomePage() {
   useEffect(() => {
     getAllUsers();
   }, []);
-  
+
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      file: null,
+    },
+  });
+
+  const handleSubmitFile = (values) => {
+    const { file } = values;
+
+    try {
+      console.log("File selected:", file);
+      const formData = new FormData();
+      formData.append("file", file);
+      // Function
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
+
   return (
     <Box>
       <Title>Home Page</Title>
+      <form onSubmit={form.onSubmit(handleSubmitFile)}>
+        <FileInput
+          label="Upload file here"
+          key={form.key("file")}
+          {...form.getInputProps("file")}
+        />
+        <Button type="submit" mt="md">
+          Submit
+        </Button>
+      </form>
     </Box>
   );
 }
