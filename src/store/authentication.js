e6d57a0abc4f84315697d7955bec502d47569faf
gabrielgatbonton <ggatbonton.cs@gmail.com>
2024-10-13 09:13:46 +0800

    Removed Persisted State

--- src/store/authentication.js
+++ src/store/authentication.js
@@ -33,7 +33,7 @@ export const useAuthenticationStore = create(
         const sub = authSubscribe((userJuno) =>
           set(() => {
             console.log(userJuno);
-            
+
             return {
               user: {
                 [userJuno.data.provider]: {
@@ -51,15 +51,15 @@ export const useAuthenticationStore = create(
 
       logoutInternetIdentity: async () => {
         const items = await listDocs({
-          collection: 'userCredentials',
+          collection: "userCredentials",
         });
-    
+
         if (items.items && items.items.length > 0 && items.items[0].data) {
           // console.log('asd', email);
           const userData = items.items[0].data;
           const userKey = items.items[0].key;
           const userVersion = items.items[0].version;
-    
+
           // use the formData when you needed to update the userCredentials.
           const updatedData = {
             dateOfBirth: userData.dateOfBirth,
@@ -67,56 +67,122 @@ export const useAuthenticationStore = create(
             fullName: userData.fullName,
             mobileNumber: userData.mobileNumber,
             password: userData.password,
-            role: userData.role
-          }
-          
+            role: userData.role,
+          };
+
           const user = await getDoc({
-            collection: 'users',
+            collection: "users",
             key: userKey,
           });
-    
+
           const updatedUserData = {
             key: user.data.key,
             fullName: user.data.fullName,
-            status: "offline"
-          }
+            status: "offline",
+          };
           set(() => ({
             user: null,
-            message: "Loading..."
-          }))
-    
+            message: "Loading...",
+          }));
+
           await setDoc({
             collection: "userCredentials",
             doc: {
               key: userKey,
               data: updatedData,
-              version: userVersion
-            }
-          })
-    
+              version: userVersion,
+            },
+          });
+
           await setDoc({
-            collection: 'users',
+            collection: "users",
             doc: {
               key: user.key,
               data: updatedUserData,
-              version: user.version
-            }
-          })
+              version: user.version,
+            },
+          });
 
           const response = await signOut();
 
           console.log(response);
 
-          sessionStorage.removeItem('authentication');
-    
-            set(() => ({
-              user: null,
-              message: 'Logout Successfully!',
-            }));
+          sessionStorage.removeItem("authentication");
+
+          set(() => ({
+            user: null,
+            message: "Logout Successfully!",
+          }));
         } else {
           set(() => ({
             user: null,
-            message: 'No account found on this identity. Please sign up first',
+            message: "No account found on this identity. Please sign up first",
+          }));
+        }
+      },
+
+      loginUser: async (email, password) => {
+        const items = await listDocs({
+          collection: "userCredentials",
+        });
+
+        if (items.items && items.items.length > 0 && items.items[0].data) {
+          // console.log('asd', email);
+          if (
+            items.items[0].data.email === email &&
+            items.items[0].data.password === password
+          ) {
+            const user = await getDoc({
+              collection: "users",
+              key: items.items[0].key,
+            });
+
+            const userData = user.data;
+            const userKey = user.key;
+            const userVersion = user.version;
+
+            // use the formData when you needed to update the userCredentials.
+            const updatedData = {
+              key: userData.key,
+              fullName: userData.fullName,
+              status: "online",
+            };
+
+            await setDoc({
+              collection: "users",
+              doc: {
+                key: userKey,
+                data: updatedData,
+                version: userVersion,
+              },
+            });
+
+            set((state) => ({
+              user: {
+                ...state.user,
+                data: {
+                  fullName: items.items[0].data.fullName,
+                  email: items.items[0].data.email,
+                },
+              },
+              message: "Login Successfully!",
+            }));
+          } else {
+            set((state) => ({
+              user: {
+                ...state.user,
+                data: null,
+              },
+              message: "Incorrect email or password",
+            }));
+          }
+        } else {
+          set((state) => ({
+            user: {
+              ...state.user,
+              data: null,
+            },
+            message: "No account found on this identity. Please sign up first",
           }));
         }
       },
