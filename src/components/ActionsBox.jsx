62ac90e66e955bd92c1ea348974e223677a00397
gabrielgatbonton <ggatbonton.cs@gmail.com>
2024-10-18 12:45:43 +0800

    Applied Reusability

--- src/components/ActionsBox.jsx
+++ src/components/ActionsBox.jsx
@@ -7,38 +7,45 @@ import {
   Popover,
   Paper,
   Divider,
+  Stack,
 } from "@mantine/core";
 
 import classes from "./ActionsBox.module.css";
 export default function ActionsBox({ options, children }) {
   const postOptions = (
-    <Paper>
-      {options.map((item, index) => {
-        const withDivider = index < options.length - 1;
-        return (
-          <UnstyledButton
-            w={125}
-            my={13}
-            key={item.value}
-            className={classes.styledButton}
-          >
-            <Group gap={10}>
-              <Image src={item.icon} w={15} h={15} fit="contain"></Image>
-              <Text size="sm" c="gray.7" fw={500}>
-                {item.label}
-              </Text>
-            </Group>
-            {withDivider && <Divider />}
-          </UnstyledButton>
-        );
-      })}
+    <Paper p={0}>
+      <Stack gap={0}>
+        {options.map((item, index) => {
+          const withDivider = index < options.length - 1;
+          return (
+            <>
+              <UnstyledButton
+                display="block"
+                py={12}
+                px={16}
+                w={150}
+                key={item.value}
+                className={classes.styledButton}
+              >
+                <Group gap={10}>
+                  <Image src={item.icon} w={15} h={15} fit="contain"></Image>
+                  <Text size="sm" c={item.textColor} fw={500}>
+                    {item.label}
+                  </Text>
+                </Group>
+              </UnstyledButton>
+              {withDivider && <Divider />}
+            </>
+          );
+        })}
+      </Stack>
     </Paper>
   );
 
   return (
     <Popover position="bottom-end">
       <Popover.Target>{children}</Popover.Target>
-      <Popover.Dropdown py={0}>{postOptions}</Popover.Dropdown>
+      <Popover.Dropdown p={0}>{postOptions}</Popover.Dropdown>
     </Popover>
   );
 }
