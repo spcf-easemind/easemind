c2a0d3f12bf31129a352581c98fbc607440053bc
gabrielgatbonton <ggatbonton.cs@gmail.com>
2024-10-18 12:30:23 +0800

    Popover Component Redesign

--- src/components/ActionsBox.jsx
+++ src/components/ActionsBox.jsx
@@ -5,42 +5,39 @@ import {
   Group,
   Text,
   Popover,
+  Paper,
+  Divider,
 } from "@mantine/core";
 
-import IconPostOption from "../assets/icons/buttons/IconPostOption.svg"
-
-import classes from "./ActionsBox.module.css"
-
-export default function ActionModal({ options }) {
-  const postOptions = options.map((item, index) => {
-    const borderStyling = index === 1 ? null : classes.borderStyling;
-    return (
-      <Box
-        key={item.value}
-        style={{
-          position: "relative",
-        }}
-      >
-        <UnstyledButton w={125} my={13}>
-          <Group gap={10}>
-            <Image src={item.icon} w={15} h={15}></Image>
-            <Text size="sm" c="var(--mantine-color-gray-7)" fw={500}>
-              {item.label}
-            </Text>
-          </Group>
-          <span className={borderStyling} />
-        </UnstyledButton>
-      </Box>
-    );
-  });
+import classes from "./ActionsBox.module.css";
+export default function ActionModal({ options, children }) {
+  const postOptions = (
+    <Paper>
+      {options.map((item, index) => {
+        const withDivider = index < options.length - 1;
+        return (
+          <UnstyledButton
+            w={125}
+            my={13}
+            key={item.value}
+            className={classes.styledButton}
+          >
+            <Group gap={10}>
+              <Image src={item.icon} w={15} h={15} fit="contain"></Image>
+              <Text size="sm" c="gray.7" fw={500}>
+                {item.label}
+              </Text>
+            </Group>
+            {withDivider && <Divider />}
+          </UnstyledButton>
+        );
+      })}
+    </Paper>
+  );
 
   return (
     <Popover position="bottom-end">
-      <Popover.Target>
-        <UnstyledButton>
-          <Image src={IconPostOption} />
-        </UnstyledButton>
-      </Popover.Target>
+      <Popover.Target>{children}</Popover.Target>
       <Popover.Dropdown py={0}>{postOptions}</Popover.Dropdown>
     </Popover>
   );
