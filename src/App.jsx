a3673d917d8ff129d45f08553ba5c956c8ed71c7
gabrielgatbonton <ggatbonton.cs@gmail.com>
2024-10-21 19:33:21 +0800

    Fixed Padding Problem

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ function App() {
 
   const withContainer = useMemo(() => {
     let withoutContainer = [];
-    const mainRoutes = ["/home", "/chat"];
+    const mainRoutes = ["/home", "/chat/:chatRef"];
     const mapNavRoutes = navRoutes.map(({ path }) => path);
 
     withoutContainer = [...mainRoutes, ...mapNavRoutes];
