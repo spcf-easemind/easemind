047f24fcc5816d79dd118aae9666f28affdcff99
Sypth <alexander.camaddo@gmail.com>
2024-10-21 15:29:50 +0800

    :wrench: Finalizing diary functions

--- src/store/diary.js
+++ src/store/diary.js
@@ -26,21 +26,22 @@ export const useDiariesStore = create((set) => ({
       let thoughtDiaryImages = [];
       const key = nanoid();
 
-      //   if (files) {
-      //     for (const file of files) {
-      //       const filename = `${key}-thoughts-${file.name}`;
-      //       const { downloadUrl } = await uploadFile({
-      //         collection: "userDiaryImages",
-      //         data: file,
-      //         filename,
-      //       });
-      //       const data = {
-      //         thoughtDiaryImagePath: downloadUrl,
-      //         fileName: filename,
-      //       };
-      //       thoughtDiaryImages.push(data);
-      //     }
-      //   }
+      if (files) {
+        for (const file of files) {
+          const filename = `${key}-thoughts-${file.name}`;
+          const { downloadUrl, fullPath } = await uploadFile({
+            collection: "userDiaryImages",
+            data: file,
+            filename,
+          });
+          const data = {
+            thoughtDiaryFullPath: fullPath,
+            thoughtDiaryImagePath: downloadUrl,
+            fileName: filename,
+          };
+          thoughtDiaryImages.push(data);
+        }
+      }
 
       if (formData) {
         const userDiary = await getDoc({
@@ -48,28 +49,27 @@ export const useDiariesStore = create((set) => ({
           key: formData.userKey,
         });
 
-        // if (userDiary) {
-        //   const currentDate = new Date();
-        //   const thoughtDiaryData = {
-        //     title: formData.thoughtDiaryInfo.title,
-        //     description: formData.thoughtDiaryInfo.description,
-        //     date: currentDate,
-        //     thoughtDiaryImages,
-        //   };
+        if (userDiary) {
+          const currentDate = new Date();
+          const thoughtDiaryData = {
+            key,
+            title: formData.thoughtDiaryInfo.title,
+            description: formData.thoughtDiaryInfo.description,
+            date: currentDate,
+            thoughtDiaryImages,
+          };
 
-        //   userDiary.data.thoughts.push(thoughtDiaryData);
+          userDiary.data.thoughts.push(thoughtDiaryData);
 
-        //   await setDoc({
-        //     collection: "userDiaries",
-        //     doc: {
-        //       key: userDiary.key,
-        //       data: userDiary.data,
-        //       version: userDiary.version,
-        //     },
-        //   });
-        // }
-
-        console.log(userDiary);
+          await setDoc({
+            collection: "userDiaries",
+            doc: {
+              key: userDiary.key,
+              data: userDiary.data,
+              version: userDiary.version,
+            },
+          });
+        }
       }
 
       set(() => ({
@@ -91,7 +91,7 @@ export const useDiariesStore = create((set) => ({
     }
   },
 
-  getUserThoughtDiaries: async (formData) => {
+  getAllUserThoughtDiaries: async (formData) => {
     set(() => ({
       diaryData: null,
       diaryMessage: "Loading...",
@@ -111,6 +111,94 @@ export const useDiariesStore = create((set) => ({
         diaryLoading: false,
       }));
       return true;
-    } catch (error) {}
+    } catch (error) {
+      console.error("Error getting user thought diary:", error);
+      set(() => ({
+        diaryData: null,
+        diaryMessage:
+          error.message || "An error occurred while getting user thought diary",
+        diaryLoading: false,
+      }));
+      return false;
+    }
+  },
+
+  getUserThoughtDiary: async (formData) => {
+    set(() => ({
+      diaryData: null,
+      diaryMessage: "Loading...",
+      diaryLoading: true,
+    }));
+
+    try {
+      const userDiary = await getDoc({
+        collection: "userDiaries",
+        key: formData.userKey,
+      });
+
+      console.log(userDiary);
+      set(() => ({
+        diaryData: diaryImages,
+        diaryMessage: "Loaded",
+        diaryLoading: false,
+      }));
+      return true;
+    } catch (error) {
+      console.error("Error getting user thought diary:", error);
+      set(() => ({
+        diaryData: null,
+        diaryMessage:
+          error.message || "An error occurred while getting user thought diary",
+        diaryLoading: false,
+      }));
+      return false;
+    }
+  },
+
+  deleteUserThoughtDiary: async (formData) => {
+    set(() => ({
+      diaryData: null,
+      diaryMessage: "Loading...",
+      diaryLoading: true,
+    }));
+
+    try {
+      const userDiary = await getDoc({
+        collection: "userDiaries",
+        key: formData.userKey,
+      });
+
+      if (userDiary) {
+        await deleteDoc({
+          collection: "userDiaries",
+          doc: userDiary,
+        });
+
+        set(() => ({
+          diaryData: null,
+          diaryMessage: "User thought diary deleted successfully!",
+          diaryLoading: false,
+        }));
+        return true;
+      } else {
+        set(() => ({
+          diaryData: null,
+          diaryMessage: "User thought diary not found!",
+          diaryLoading: false,
+        }));
+        return false;
+      }
+      return true;
+    } catch (error) {
+      console.error("Error deleting user thought diary:", error);
+      set(() => ({
+        diaryData: null,
+        diaryMessage:
+          error.message ||
+          "An error occurred while deleting user thought diary",
+        diaryLoading: false,
+      }));
+      return false;
+    }
   },
 }));
